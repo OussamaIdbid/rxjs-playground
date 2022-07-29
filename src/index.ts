7266750568931faa8cf30f6a6af6c6f3bde9d5c7
Oussama Idbid <oussama@surelock.nl>
2022-07-29 12:58:29 +0200

    progress

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
-import { Observable, Subscriber } from "rxjs";
+import { Observable } from "rxjs";
 
-const observable$ = new Observable<string>((subscriber) => {
-  subscriber.next("bruhmoment");
-  subscriber.next("maainn you done f'ed up");
-  subscriber.next("me oh my what do we have here");
+const observable$ = new Observable((subscriber) => {
+  let count = 0;
+  const interval = setInterval(() => {
+    console.log("emitted val", count);
+
+    subscriber.next(count++);
+  }, 2000);
+
+  return () => {
+    clearInterval(interval);
+  };
 });
 
-const observer = {
-  next: (val: string) => console.log(val),
-  err: (err: string) => console.log(err),
-};
+const observer = observable$.subscribe({
+  next: (val) => console.log(val),
+});
 
-const observer1 = {
-  next: (val: string) => console.log("look at these values bruh", val),
-};
-
-const subscription = observable$.subscribe(observer);
-const subscription1 = observable$.subscribe(observer1);
-
-setTimeout(() => subscription.unsubscribe(), 3000);
+setTimeout(() => observer.unsubscribe(), 7000);
